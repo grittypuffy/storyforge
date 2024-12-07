704ebc17c93f72fc12a130eddf7ca26e6803d9ab
grittypuffy <grittypuffy@riseup.net>
2024-12-07 16:53:18 +0530

    Add: Project creation page

---
+++ src/utils/input/styles.ts
@@ -0,0 +1,23 @@
+const inputStyles = {
+        label: "text-black/80 dark:text-white/90",
+        input: [
+          "bg-transparent",
+          "text-black/90 dark:text-white/90",
+          "placeholder:text-black/50 dark:placeholder:text-white/60",
+        ],
+        innerWrapper: "bg-transparent",
+        inputWrapper: [
+          "shadow-xl",
+          "bg-slate-100",
+          "dark:bg-default/20",
+          "backdrop-blur-xl",
+          "backdrop-saturate-200",
+          "hover:bg-default/70",
+          "dark:hover:bg-default/30",
+          "group-data-[focus=true]:bg-default-200/50",
+          "dark:group-data-[focus=true]:bg-default/60",
+          "!cursor-text",
+        ],
+      }
+
+export default inputStyles;
---
+++ src/utils/project/genres.ts
@@ -0,0 +1,14 @@
+const genres = [
+    {key: "adventure", label: "Adventure"},
+    {key: "comedy", label: "Comedy"},
+    {key: "moral", label: "Moral"},
+    {key: "mystery", label: "Mystery"},
+    {key: "non-fic", label: "Non-fiction"},
+    {key: "philosophy", label: "Philosophy"},
+    {key: "romance", label: "Romance"},
+    {key: "sci-fi", label: "Science Fiction"},
+    {key: "spirituality", label: "Spirituality"},
+    {key: "technology", label: "Technology"}
+  ];
+
+export default genres;
