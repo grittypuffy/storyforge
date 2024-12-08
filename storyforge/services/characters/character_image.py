15e66eed9da1790e71d80c2e01f152e00d0075c8
Arun Pranav A T <pranav9176@gmail.com>
2024-12-08 12:07:53 +0530

    tried adding the routes and reframed the services

--- storyforge/services/characters/character_image.py
+++ storyforge/services/characters/character_image.py
@@ -1,11 +1,6 @@
 # Service for generating character image for a given character
 
-# importing necessary libraries
-from optimum.intel import OVLatentConsistencyModelPipeline
-
-# model id for the cover image generation model
-model_id = "OpenVINO/LCM_Dreamshaper_v7-int8-ov"
-pipeline = OVLatentConsistencyModelPipeline.from_pretrained(model_id)
+from services.imagemodel import pipeline
 
 # function to generate a character image for the given character
 # character_name: str, name of the character
--- storyforge/services/project/cover_image.py
+++ storyforge/services/project/cover_image.py
@@ -1,11 +1,6 @@
 # Service for generating cover image for a given story
 
-# importing necessary libraries
-from optimum.intel import OVLatentConsistencyModelPipeline
-
-# model id for the cover image generation model
-model_id = "OpenVINO/LCM_Dreamshaper_v7-int8-ov"
-pipeline = OVLatentConsistencyModelPipeline.from_pretrained(model_id)
+from services.imagemodel import pipeline
 
 # function to generate a cover image for the given story
 # story_title: str, title of the story
